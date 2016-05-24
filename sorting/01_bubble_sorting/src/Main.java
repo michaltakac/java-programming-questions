1cea0861cb74247559dc6788dfd6bb689ade78a8
Michal Takac <takacmichal92@gmail.com>
2016-05-24 14:27:27 +0200

    Bubble sort, ascending and descending

--- sorting/01_bubble_sorting/src/Main.java
+++ sorting/01_bubble_sorting/src/Main.java
@@ -1,6 +1,50 @@
+import java.util.Arrays;
+
 public class Main {
 
+    /**
+     * Bubble sort.
+     *
+     * @param numbers Array<Int> Numbers to sort
+     * @param type String type of sorting (asc|desc)
+     */
+    public static void BubbleSort(int[] numbers, String type) {
+        System.out.print("Your array input: " + Arrays.toString(numbers) +"\n\n");
+        int i;
+        // Setting flag to true to start sorting
+        boolean flag = true;
+        // Temporary placeholder
+        int temp;
+
+        while (flag) {
+            flag = false;
+            if (type == "asc") {
+                for (i = 0; i < numbers.length-1; i++) {
+                    if (numbers[i] > numbers[i+1]) {
+                        temp = numbers[i];
+                        numbers[i] = numbers[i+1];
+                        numbers[i+1] = temp;
+                        flag = true;
+                    }
+                }
+            } else {
+                for (i = 0; i < numbers.length-1; i++) {
+                    if (numbers[i] < numbers[i+1]) {
+                        temp = numbers[i];
+                        numbers[i] = numbers[i+1];
+                        numbers[i+1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        // Print out sorted array
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Call bubble sort method
+        BubbleSort(new int[]{34, 65, 1, 55, 98, 45}, "desc");
     }
 }
---
+++ sorting/README.md
@@ -0,0 +1,9 @@
+# Bubble sort
+
+In the bubble sort, as elements are sorted they gradually "bubble" (or rise) to their proper 
+location in the array, like bubbles rising in a glass of soda. The bubble sort repeatedly 
+compares adjacent elements of an array. The first and second elements are compared and swapped 
+if out of order.  Then the second and third elements are compared and swapped if out of order.  
+This sorting process continues until the last two elements of the array are compared and swapped 
+if out of order.
+
